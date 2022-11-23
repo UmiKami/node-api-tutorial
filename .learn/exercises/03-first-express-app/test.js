f16daabe50243d11c36770ff289c1d7971919a67
ErnestoXG <ernestox.061@gmail.com>
2022-11-23 01:17:01 +0000

    added test unit test

---
+++ .learn/exercises/03-first-express-app/test.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const rewire = require('rewire');
+
+test('app.js file should exist in your root directory', () => {
+    expect(2+2).toEqual(4)
+})
