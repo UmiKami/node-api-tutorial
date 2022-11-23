5b295a165a0e4955e7aae619ae6c84e4af450efd
ErnestoXG <ernestox.061@gmail.com>
2022-11-23 23:01:33 +0000

    added partial test, crashes

--- .learn/exercises/03-first-express-app/test.js
+++ .learn/exercises/03-first-express-app/test.js
@@ -3,5 +3,41 @@ const path = require('path');
 const rewire = require('rewire');
 
 test('app.js file should exist in your root directory', () => {
-    expect(2+2).toEqual(4)
+    let fileExists = false;
+
+    if(fs.existsSync(path.resolve('./app.js'))){
+        fileExists = true;
+    }
+
+    expect(fileExists).toBeTruthy();
+})
+
+test('express dependency should be required', () => {
+    const file = fs.readFileSync(path.resolve('./app.js'), 'utf8');
+
+    const regex = /(let|const)\s*express\s*=\s*require\s*\(\s*'express'\s*\)/gm
+
+    expect(regex.test(file.toString())).toBeTruthy();
+})
+
+test('should should initialize your express app', () => {
+    const file = fs.readFileSync(path.resolve('./app.js'), 'utf8');
+
+    const regex = /(let|const)\s*app\s*=\s*express\s*\(\s*\)/gm
+
+    expect(regex.test(file.toString())).toBeTruthy();
+})
+
+test('your app should be using express.json()', () => {
+    const file = fs.readFileSync(path.resolve('./app.js'), 'utf8');
+
+    const regex = /app\.use\s*\(\s*express\.json\s*\(\s*\)\s*\)/gm
+
+    expect(regex.test(file.toString())).toBeTruthy();
+})
+
+test('your server should be opening with port 8080', () => {
+    const file = rewire(path.resolve('./app.js'))
+
+    expect(app.listen).toHaveBeenCalledWith(8080)
 })
